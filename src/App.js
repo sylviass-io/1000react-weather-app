65e8f8dc2549c0cc0b200e810c8f20ee4ac331c2
jakobi007 <sylwia.salapa@gmail.com>
2023-07-06 10:23:54 +0100

    Updated App.js by adding footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Sylvia SS and is{" "}
+        <a
+          href="https://github.com/sylviass-io/1000react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }

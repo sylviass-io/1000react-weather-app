6dd6e4a3e655b7a62fb3f3699e037d2106b74704
jakobi007 <sylwia.salapa@gmail.com>
2023-07-06 11:19:09 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Sylvia SS and is{" "}
-        <a
-          href="https://github.com/sylviass-io/1000react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Sylvia SS and is{" "}
+          <a
+            href="https://github.com/sylviass-io/1000react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }

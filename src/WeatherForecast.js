d0da55aa7a4361041100e38c9d07476418b18595
jakobi007 <sylwia.salapa@gmail.com>
2023-07-14 10:07:55 +0100

    Added a 5 day forecast using useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -17,7 +21,19 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>{" "}
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>{" "}
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
+          </div>{" "}
+          <div className="col">
+            <WeatherForecastDay data={forecast[5]} />
           </div>
         </div>
       </div>
